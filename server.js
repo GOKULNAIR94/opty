abfb3da843a2670f064ac2111321d7f906ce83e3
GOKULNAIR94 <gokulgnair94@gmail.com>
2017-05-24 12:22:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 var http = require('http');
 var fs = require('fs'),
 express = require('express'),
@@ -11,110 +12,66 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 
 
-var varAuth = 'Basic YWthc2hkZWVwOmxudExOVDJLMTZfMQ==';
-var varHost = 'acs.crm.ap2.oraclecloud.com';
-var varPath = '/salesApi/resources/latest/Task_SA_c';
+var urlPath = '';
 const https = require('https');
-
+var uname = 'kaamana';
+var pword = 'Oracle1234';
+var request;
+var responseString;
+var resCode = '';
+var resObj = '';
+var options='';
+var speech = '';
 
 app.post('/inputmsg',function(request,response){
-	console.log('Req : '+ request.body.result.parameters.any);
 	
-	//Importing Data from OSC
-	//console.log(" RecordName :" + request.body.RecordName);
-    var recordNameURL = (request.body.result.parameters.any).trim().replace( / /g, "%20" );
-    console.log("recordNameURL :" + recordNameURL)
-	var options = {
-		host: varHost,
-		port: 443,
-		path: varPath + "/?q=RecordName=" + recordNameURL, 
-		headers: {
-		'Authorization': varAuth,
-		'Content-Type': 'application/vnd.oracle.adf.resourceitem+json'
-		}   
-	};
-	var responseObject;
-	var r = https.get(options, function(res){
-	var body = "";
-
-	res.on('data', function(data) {
-		body += data;
-	});
-	res.on('end', function() {
-		
-		responseObject = JSON.parse(body);
-		//response.json(responseObject);
-        //console.log(" Object : " + body);
-                console.log(" Attribute : " + (request.body.result.parameters.attribute));
-	        //console.log(" Value : " + responseObject.items[0]['Id']); //VAR APNAvAR = "Id";   //responseObject.items[0][apnavar]
-		
-		
-		if(request.body.result.parameters.attribute){
-			var answer = responseObject.items[0][request.body.result.parameters.attribute];
-			console.log(" Value : " + answer);
-			if(request.body.result.parameters.attribute=="AssignedTo_SA2_c")
-			{
-				var speech = "The task "+recordNameURL+" is assigned to "+ answer+".";
-			}
-			else if(request.body.result.parameters.attribute=="Status_SA_c")
-			{
-				var speech = "The status of "+recordNameURL+" is "+ answer+".";
-			}
-			else if(request.body.result.parameters.attribute=="Type_c")
-			{
-				var speech = "The type of "+recordNameURL+" is "+ answer+".";
-			}
-			else if(request.body.result.parameters.attribute=="Id")
-			{
-				var speech = "The Id of "+recordNameURL+" is "+ answer+".";
-			}
-			else if(request.body.result.parameters.attribute=="CreatedBy")
-			{
-				var speech = "This "+recordNameURL+" is  created by "+ answer+".";
-			}
-			else if(request.body.result.parameters.attribute=="EmailAddress_c")
-			{
-				var speech = answer+" is associated to "+recordNameURL;
-			}
-			else if(request.body.result.parameters.attribute=="CreationDate")
-			{
-				var speech = recordNameURL+" was created on "+answer;
-			}
-			else if(request.body.result.parameters.attribute=="LastUpdateDate")
-			{
-				var speech = recordNameURL+" was last updated on "+answer;
-			}
-			else if(request.body.result.parameters.attribute=="LastUpdatedBy")
-			{
-				var speech = recordNameURL+" was recently updated by "+answer;
-			}
-			else
-			{
-				var speech = "Your answer is "+ answer+".";
-			}	
+	titleName = req.body.result.parameters.titleName;
+	//titleName = encodeURIComponent(titleName);
+	urlPath = '/salesApi/resources/latest/Title_c?onlyData=true&q=TitleName_c=' + titleName + '&fields=TitleNumber_c';
+	
+	options = 
+	{
+		host: 'cbhs-test.crm.us2.oraclecloud.com',
+		path: urlPath,
+		headers: 
+		{
+			'Authorization': 'Basic ' + new Buffer(uname + ':' + pword).toString('base64')
 		}
-		else{
-				var speech = "Couldnt find your answer";
-		}			     
-		return response.json({
-			speech: speech,
-			displayText: speech,
-			source: 'poc4'
+	};
+	request = http.get(options, function(resg){
+		responseString = "";
+      	
+		resg.on('data', function(data) 
+		{
+			responseString += data;
 		});
-		// console.log(" Value : " + answer);
-        //response.json(responseObject.items[0][request.body.attribute]);
-
-		})
-	}).on('error', function(e){
-	console.error(e);
-  });
-
-	request.on('error', function(error) {
-		console.log(error);
+		
+		resg.on('end', function(){
+			resCode = responseString;
+            try
+            {
+                resObj=JSON.parse(resCode);
+				console.log(resObj);               
+                tNumber=resObj.items[0].TitleNumber_c;
+				console.log(tNumber);
+				return res.json
+			            ({
+			                speech: speech,
+			                displayText: speech,
+			                //source: 'webhook-OSC-oppty'
+			            })
+            }
+            catch (error)
+            {
+                return res.json
+                ({	
+                    speech: 'Incorrect title name'
+                })
+               console.log('Got ERROR');
+            }
+		});
+		
 	});
-	
-	//request.end();
-	
 });
 
 
@@ -130,3 +87,8 @@ function send404(response){
 app.listen((process.env.PORT || 8000), function() {
     console.log("Server up and listening");
 });
+
+    Contact GitHub API Training Shop Blog About 
+
+    © 2017 GitHub, Inc. Terms Privacy Security Status Help 
+
