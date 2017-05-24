73aca3ffdef804f9f2994b72f4bcb995f71f7a06
GOKULNAIR94 <gokulgnair94@gmail.com>
2017-05-24 23:49:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,11 +28,11 @@ restService.post('/inputmsg', function(req, res)
 {
   titleName = req.body.result.parameters.titleName;
   territoryStored = req.body.result.parameters.territoryStored;
+  console.log( "titleName :" + titleName);
+  console.log( " territoryStored : " + territoryStored);
   if( territoryStored != null )
      myContext = 'multiTerritory';
     
-  console.log(titleName);
-  
   switch( myContext )
   {
     case "start":
@@ -44,205 +44,11 @@ restService.post('/inputmsg', function(req, res)
       MultiTerritory()
       break;
   }
-      
-function Start()
-{
-  urlPath='/salesApi/resources/latest/Title_c?onlyData=true&q=TitleName_c=' + encodeURIComponent(titleName) + '&fields=TitleNumber_c'; 
-  console.log(urlPath);
-  
-  options = 
-  {
-    host: 'cbhs-test.crm.us2.oraclecloud.com',
-    path: urlPath,
-    headers: 
-    {
-      'Authorization': 'Basic ' + new Buffer(uname + ':' + pword).toString('base64')
-    }
-  };
-  request = http.get(options, function(resg)
-    {
-        responseString = "";
-        resg.on('data', function(data) 
-    {
-      responseString += data;
-    });
-        resg.on('end', function() 
-        {
-            resCode = responseString;
-            try
-            {
-                resObj = JSON.parse(resCode);
-        //console.log(resObj);               
-                tNumber = resObj.items[0].TitleNumber_c;
-        console.log(tNumber);
-            }
-            catch (error)
-            {
-                return res.json
-                ({
-                    speech: 'Incorrect title name'
-                })
-               console.log('Got ERROR');
-            }
-              
-            if( territoryStored == null)
-                urlPath='/salesApi/resources/latest/__ORACO__PromotionProgram_c?onlyData=true&q=TitleNumberStored_c='+ tNumber + '&fields=RecordName,Id'; 
-            else
-                urlPath='/salesApi/resources/latest/__ORACO__PromotionProgram_c?onlyData=true&q=TitleNumberStored_c='+ tNumber + ';TerritoryStored_c='+territoryStored+'&fields=RecordName,Id'; 
-    
-            console.log( urlPath );
-            
-            options = 
-            {
-              host: 'cbhs-test.crm.us2.oraclecloud.com',
-              path: urlPath,
-              headers: 
-              {
-                'Authorization': 'Basic ' + new Buffer(uname + ':' + pword).toString('base64')
-              }
-            };
-            
-        request = http.get(options, function(resx)
-        {
-          responseString = "";
-          resx.on('data', function(data) 
-          {
-            responseString += data;
-          });
-          resx.on('end', function() 
-          {
-            resObj=JSON.parse(responseString);
-            //tNumber=resObj.items[0].TitleNumber_c;
-            //console.log(resObj);
-            var promoCount = resObj.count
-            console.log( "promoCount : " + promoCount);
-            speech = "";
-            speech= 'There are ' + promoCount + ' promotion(s) for the Title ' + titleName + "\n Please select a region of the Promotion of the Title";
-            var pId, pName;
-            for( var i =0; i< promoCount; i++)
-            {
-              pId = resObj.items[i].Id;
-              pName = resObj.items[i].RecordName;
-              speech = speech + "\n\n" + parseInt(i+1,10) + ". " + pId + " - " + pName;
-              if( i == promoCount - 1 )
-                speech = speech + ".";
-              else
-                speech = speech + ",";  
-            }
-            //speech= 'There are ' + promoCount + ' promotions for the Title ' + titleName;
-            console.log(speech);
-            return res.json
-                  ({
-                      speech: speech,
-                      displayText: speech,
-                      //source: 'webhook-OSC-oppty'
-                  })
-          });
-          resx.on('error', function(e) 
-          {
-            console.log("Got error: " + e.message);
-          });
-        });
-        })
-    
-        resg.on('error', function(e) 
-        {
-            console.log('Got error: ' + e.message);
-        });
-    }); 
-  
-}
-function MultiTerritory(){
-  urlPath='/salesApi/resources/latest/__ORACO__PromotionProgram_c?onlyData=true&q=TitleNumberStored_c='+ tNumber + ';TerritoryStored_c='+territoryStored+'&fields=RecordName,Id'; 
-  options = 
-          {
-            host: 'cbhs-test.crm.us2.oraclecloud.com',
-            path: urlPath,
-            headers: 
-            {
-              'Authorization': 'Basic ' + new Buffer( uname + ':' + pword ).toString('base64')
-            }
-          };
-  var pId, pName;
-  request = http.get(options, function(resx)
-        {
-          responseString = "";
-          resx.on('data', function(data) 
-          {
-            responseString += data;
-          });
-          resx.on('end', function() 
-          {
-          
-            resObj=JSON.parse(responseString);
-            
-            
-            
-            pId=resObj.items[0].Id;
-            pName=resObj.items[0].RecordName;
-            speech =  pId + " - " + pName;
 
-//            return res.json
-//                  ({
-//                      speech: speech,
-//                      displayText: speech,
-//                      //source: 'webhook-OSC-oppty'
-//                  })
-          });
-          resx.on('error', function(e) 
-          {
-            console.log("Got error: " + e.message);
-          });
-    
-    options.path = "/salesApi/resources/latest/MarketSpend_c?onlyData=true&q=PromotionName_Id_c=" + pId + "&fields=Id,RecordName,Status_c,RequestType_c";
-    request = http.get(options, function(resx)
-        {
-          responseString = "";
-          resx.on('data', function(data) 
-          {
-            responseString += data;
-          });
-          resx.on('end', function() 
-          {
-          
-            resObj=JSON.parse(responseString);
-            var msId, msName;
-            var msCount = resObj.count
-            console.log( "msCount : " + msCount);
-            speech = "";
-            speech= 'There are ' + msCount + ' MS(s) for the Title ' + msName + "\n Please select a ms";
-            for( var i =0; i< msCount; i++)
-            {
-              msId = resObj.items[i].Id;
-              msName = resObj.items[i].RecordName;
-              speech = speech + "\n\n" + parseInt(i+1,10) + ". " + msId + " - " + msName;
-              if( i == msCount - 1 )
-                speech = speech + ".";
-              else
-                speech = speech + ",";  
-            }
-            
-            console.log(speech);
-            return res.json
-                  ({
-                      speech: speech,
-                      displayText: speech,
-                      //source: 'webhook-OSC-oppty'
-                  })
-          });
-          resx.on('error', function(e) 
-          {
-            console.log("Got error: " + e.message);
-          });
-
-        });
-    });
-  
-}
-        
-});
-
-
-restService.listen((process.env.PORT || 9000), function() {
-    console.log("Server up and listening");
+  function Start(){
+    console.log("Start");
+  }
+  function MultiTerritory(){
+    console.log("MultiTerritory");
+  }
 });
