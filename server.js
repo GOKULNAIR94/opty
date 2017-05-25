b4de9dc752424c0e1baa0d7212bd67473d387a8b
GOKULNAIR94 <gokulgnair94@gmail.com>
2017-05-25 19:02:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -109,7 +109,7 @@ try
 		console.log("tNumber : " + tNumber);
       
 	  
-      urlPath='/salesApi/resources/latest/__ORACO__PromotionProgram_c?onlyData=true&q=TitleNumberStored_c='+ tNumber + ';TerritoryStored_c=Global&fields=RecordName,Id'; 
+      urlPath='/salesApi/resources/latest/__ORACO__PromotionProgram_c?onlyData=true&q=TitleNumberStored_c='+ tNumber + '&fields=RecordName,Id'; 
       query( urlPath, function(result) {
       var promoCount = result.count;
       console.log( "promoCount : " + promoCount);
