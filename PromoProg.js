4d08fd9467eec8c09b7fa8f97a376163beaf0417
Gokul Nair <Goku@Gokuls-MacBook-Air.local>
2017-05-28 17:53:15 +0530

    get promo with territory

--- PromoProg.js
+++ PromoProg.js
@@ -6,11 +6,14 @@ module.exports = function PromoProg( req, res, callback ) {
     var objectName = '';
     var territoryStored = '';
     var tNumber = '';
+    var attributeName = '';
+    var speech = '';
 
     titleName = req.body.result.parameters.titleName;
     territoryStored = req.body.result.parameters.Territory;
     objectName = req.body.result.parameters.object;
     actionType = req.body.result.parameters.actionType;
+    attributeName = req.body.result.parameters.PPattributes;
 
     console.log( "titleName : " + titleName );
     
@@ -22,9 +25,15 @@ module.exports = function PromoProg( req, res, callback ) {
         
         if( territoryStored != null ){
             console.log( "Territory : " + territoryStored );
-            urlPath = '/salesApi/resources/latest/__ORACO__PromotionProgram_c?onlyData=true&q=TitleNumberStored_c=' + tNumber + ';TerritoryStored_c=' + territoryStored + '&fields=RecordName,Id';
+            urlPath = '/salesApi/resources/latest/__ORACO__PromotionProgram_c?onlyData=true&q=TitleNumberStored_c=' + tNumber + ';TerritoryStored_c=' + territoryStored;
             Query( req, res, urlPath, function( result ) {
                 console.log( "result : " + result);
+                speech = attributeName + " of " + result.items[0].RecordName + " : " + result.items[0][attributeName];
+                res.json({
+                    speech: speech,
+                    displayText: speech,
+                    //source: 'webhook-OSC-oppty'
+                })
             });
         }
     });
