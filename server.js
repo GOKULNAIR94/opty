c7a457c555d3d657f0e5025c9ec88d57ce2e3ca6
GOKULNAIR94 <gokulgnair94@gmail.com>
2017-05-28 15:46:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,29 +9,8 @@ restService.use(bodyParser.urlencoded({
     extended: true
 }));
 restService.use(bodyParser.json());
-var myContext = 'getPromo';
-var actionType = "";
-var titleName = '';
-var tNumber = '';
-var territoryStored = '';
-var objectName = '';
-var attributeName = '';
-var msRecord = '';
-var newValue = "";
-var msId = "";
-var outputAttribute ="";
 
-var uname = 'gokuln';
-var pword = 'Goklnt@1';
-var speech = '';
-var options = '';
-var urlPath = '';
-var request;
-var responseString;
-var resCode = '';
-var resObj = '';
-var pId, pName, msId, msName;
-
+ver funcky = require('./index');
 
 restService.post('/inputmsg' {
 	funcky.init(restService);
