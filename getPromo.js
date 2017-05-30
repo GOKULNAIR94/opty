d631d035f2aa773b11b01427d8b4227a61f14415
Gokul Nair <Goku@Gokuls-MacBook-Air.local>
2017-05-30 22:56:27 +0530

    Without Territory

--- getPromo.js
+++ getPromo.js
@@ -27,13 +27,12 @@ module.exports = function getPromo( req, res, callback ) {
         console.log( "result : " + result);
         tNumber = result.items[0].TitleNumber_c;
         console.log("tNumber : " + tNumber);
-        urlPath = '/salesApi/resources/latest/__ORACO__PromotionProgram_c?onlyData=true&q=TerritoryStored_c=' + territoryStored + ';TitleNumberStored_c=' + tNumber;
-        Query( req, res, urlPath, function( result ) {
-            var promoCount = result.count;
-            console.log("promoCount : " + promoCount);
-            speech = "";
-            
-            if( promoCount == 1 ){
+        
+        if( territoryStored != null && territoryStored != "" ){
+            console.log( "Territory not null: " + territoryStored );
+            urlPath = '/salesApi/resources/latest/__ORACO__PromotionProgram_c?onlyData=true&q=TitleNumberStored_c=' + tNumber + ';TerritoryStored_c=' + territoryStored;
+            Query( req, res, urlPath, function( result ) {
+                console.log( "actionType : " + actionType);
                 if( actionType == "update" ){
                     var bodyToUpdate = {};
                     var newValue = req.body.result.parameters.newValue;
@@ -47,11 +46,34 @@ module.exports = function getPromo( req, res, callback ) {
                     callback( result );
                 }
                 
-            }
-            if( promoCount > 1 ){
-                callback( result );
-            }
-        });
-        
+            });
+        }
+        else{
+            urlPath = '/salesApi/resources/latest/__ORACO__PromotionProgram_c?onlyData=true&q=TitleNumberStored_c=' + tNumber  + '&fields=RecordName,Id';
+            Query( req, res, urlPath, function( result ) {
+                var promoCount = result.count;
+                console.log("promoCount : " + promoCount);
+                speech = "";
+                
+                if( promoCount == 1 ){
+                    if( actionType == "update" ){
+                        var bodyToUpdate = {};
+                        var newValue = req.body.result.parameters.newValue;
+                        bodyToUpdate[attributeName] = newValue;
+                        urlPath = '/salesApi/resources/latest/__ORACO__PromotionProgram_c/' + result.items[0].Id;
+                        Update( req, res, urlPath, bodyToUpdate, function( result ) {
+                            console.log( "Value Updated : " + result);
+                        });
+                    }
+                    else{
+                        callback( result );
+                    }
+                    
+                }
+                if( promoCount > 1 ){
+                    callback( result );
+                }
+            });
+        }
     });
 }
