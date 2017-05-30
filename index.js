e4596d3227846fd54e1c8e4c886ecf9f0b6d5a6a
Gokul Nair <Goku@Gokuls-MacBook-Air.local>
2017-05-30 20:18:11 +0530

    New Arch

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = function(req, res) {
     
     var objectName = '';
     var PromoProg = require( "./PromoProg" );
+    var MarketSpend = require( "./MarketSpend" );
     console.log( "In Query!" );
 
     objectName = req.body.result.parameters.objectName;
@@ -10,6 +11,20 @@ module.exports = function(req, res) {
     switch ( objectName ) {
         case "__ORACO__PromotionProgram_c":
             PromoProg( req, res, function( result ) {
+                console.log( "result : " + result);
+                var ogAttribute = req.body.result.parameters["PPattributes"];
+                var attributeName = req.body.result.parameters.PPattributes;
+                speech = ogAttribute + " of " + result.items[0].RecordName + " : " + result.items[0][attributeName];
+                res.json({
+                    speech: speech,
+                    displayText: speech,
+                    //source: 'webhook-OSC-oppty'
+                })
+            });
+            break;
+
+        case "MarketSpend_c":
+            MarketSpend( req, res, function( result ) {
                 console.log( "result : " + result);
             });
             break;
