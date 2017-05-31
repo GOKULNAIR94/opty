c81d994f8a153c057a027f44bac2f71e26c5190e
GOKULNAIR94 <gokulgnair94@gmail.com>
2017-05-31 12:49:13 +0530

    AJv2

--- MarketSpend.js
+++ MarketSpend.js
@@ -7,6 +7,9 @@ module.exports = function MarketSpend( req, res, callback ) {
     var attributeName = req.body.result.parameters.PPattributes;
     var titleName = req.body.result.contexts[0].parameters["titleName.original"];
     var ogAttribute = req.body.result.contexts[0].parameters["PPattributes.original"];
+    
+    var msId;
+    var msName;
 
     getPromo( req, res, function( result ) {
         console.log( "result : " + result);
@@ -19,8 +22,33 @@ module.exports = function MarketSpend( req, res, callback ) {
         if( promoCount == 1 ){
             pId = result.items[0].Id;
             pName = result.items[0].RecordName;
-            getObject( req, res, function( result ) {
+            getObject( pId, req, res, function( result ) {
+                
+                var msCount = result.count;
+                if( msCount == 1 ){
+                    msId = result.items[0].Id;
+                    msName = result.items[0].RecordName;
+                    speech = ogAttribute + " of " + result.items[0].RecordName + " : " + result.items[0][attributeName];
+                }
 
+                if( msCount > 1 ){
+                    speech = 'There are ' + msCount + ' records(s) for the Promotion ' + pName + "\n Please select a record.";
+                    
+                    for (var i = 0; i < msCount; i++) {
+                        msId = result.items[i].Id;
+                        msName = result.items[i].RecordName;
+                        speech = speech + "\n\n" + parseInt(i + 1, 10) + msName;
+                        if (i == promoCount - 1)
+                            speech = msCount + ".";
+                        else
+                            speech = speech + ",";
+                    }
+                }
+//                res.json({
+//                    speech: speech,
+//                    displayText: speech,
+//                    //source: 'webhook-OSC-oppty'
+//                })
             });
             speech = ogAttribute + " of " + result.items[0].RecordName + " : " + result.items[0][attributeName];
         }
--- getObject.js
+++ getObject.js
@@ -1,3 +1,43 @@
-module.exports = function getObject( req, res, callback ) {
+module.exports = function getObject( pId, req, res, callback ) {
+    var Query = require( "./query" );
+    
+    var actionType = "";
+    var titleName = '';
+    var objectName = '';
+    var territoryStored = '';
+    var tNumber = '';
+    var attributeName = '';
+    var speech = '';
+    
+    titleName = req.body.result.parameters["titleName"];
+    territoryStored = req.body.result.parameters.Territory;
+    objectName = req.body.result.parameters.object;
+    actionType = req.body.result.parameters.actionType;
+    attributeName = req.body.result.parameters.PPattributes;
+    
+    urlPath = "/salesApi/resources/latest/MarketSpend_c?onlyData=true&q=PromotionName_Id_c=" + pId + "&fields=Id,RecordName";
+    Query( req, res, urlPath, function( result ) {
+        var promoCount = result.count;
+        console.log("promoCount : " + promoCount);
+        speech = "";
 
+        if( promoCount == 1 ){
+            if( actionType == "update" && req.body.result.parameters.objectName == "__ORACO__PromotionProgram_c" ){
+                var bodyToUpdate = {};
+                var newValue = req.body.result.parameters.newValue;
+                bodyToUpdate[attributeName] = newValue;
+                urlPath = '/salesApi/resources/latest/__ORACO__PromotionProgram_c/' + result.items[0].Id;
+                Update( req, res, urlPath, bodyToUpdate, function( result ) {
+                    console.log( "Value Updated : " + result);
+                });
+            }
+            else{
+                callback( result );
+            }
+
+        }
+        if( promoCount > 1 ){
+            callback( result );
+        }
+    });
 }
