bb31c4653dbf1474769369457e3bbc8948bf7c9f
GOKULNAIR94 <gokulgnair94@gmail.com>
2017-06-01 11:28:36 +0530

    1_6

--- PromoProg.js
+++ PromoProg.js
@@ -23,6 +23,7 @@ module.exports = function PromoProg( req, res, callback ) {
             res.json({
 	            speech: speech,
 	            displayText: speech,
+                contextOut: [{"name":"action2", "lifespan":1, "parameters":{"titleName.original": titleName , "PPattributes.original" :ogAttribute }}]
 	            //source: 'webhook-OSC-oppty'
 	        });
         }
