88f290ef4f9bbf980dd077b62b62c5a845814c27
GOKULNAIR94 <gokulgnair94@gmail.com>
2017-06-01 11:46:10 +0530

    Update getPromo.js

--- getPromo.js
+++ getPromo.js
@@ -4,6 +4,7 @@ module.exports = function getPromo( req, res, callback ) {
     var Update = require( "./update" );
     
     var actionType = "";
+    var actionType1 = "";
     var titleName = '';
     var objectName = '';
     var territoryStored = '';
@@ -17,6 +18,7 @@ module.exports = function getPromo( req, res, callback ) {
     territoryStored = req.body.result.parameters.Territory;
     objectName = req.body.result.parameters.object;
     actionType = req.body.result.parameters.actionType;
+    actionType1 = req.body.result.parameters.actionType1;
     attributeName = req.body.result.parameters.PPattributes;
     
 
@@ -34,7 +36,7 @@ module.exports = function getPromo( req, res, callback ) {
             speech = "";
             
             if( promoCount == 1 ){
-                if( actionType == "update" && req.body.result.parameters.objectName == "__ORACO__PromotionProgram_c" ){
+                if( actionType1 == "update" && req.body.result.parameters.objectName == "__ORACO__PromotionProgram_c" ){
                     var bodyToUpdate = {};
                     var newValue = req.body.result.parameters.newValue;
                     bodyToUpdate[attributeName] = newValue;
