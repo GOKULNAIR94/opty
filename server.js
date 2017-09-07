d5ae68ae7b91aba6cf37a836f1fb04efb263f96c
Gokul Nair <gokulgnair94@gmail.com>
2017-09-07 22:12:28 +0530

    Done

--- server.js
+++ server.js
@@ -89,8 +89,10 @@ restService.post('/oppty', function(req, res) {
                     console.log("loginEncoded2 : " + loginEncoded2);
 
                     console.log("WTF");
+                    console.log(req.body.result.parameters.opptyNumber);
+                    console.log("WTF2");
                     oNumber = req.body.result.parameters.opptyNumber;
-                    console.log("req.body.result.parameters.opptyNumber");
+                    
                     var prob = req.body.result.parameters.Probability;
                     var actionType = req.body.result.parameters.actionType;
                     
