403ef76f11fdb38d3441a46f99b2c37f2a770af4
Gokul Nair <gokulgnair94@gmail.com>
2017-10-03 00:27:18 +0530

    Top Opty

--- server.js
+++ server.js
@@ -819,8 +819,8 @@ restService.post('/opptytop', function(req, res) {
                 QueryOpty( qString, loginEncoded, req, res, function( result ){
                     
                     //console.log( "result : " + JSON.stringify(result));
-                    speech = "Opportunity Name: " + result.Name +" ,  Account : " + result.TargetPartyName + ". The customer " + result.TargetPartyName + " is at high risk. Would you like to know more details like revenue, churn index or what is in the news about the account?";
-                    var suggests = [{ "title" : "What is the revenue"},{ "title" : "What is the churn index"},{ "title" : "What is in the news"}];
+                    speech = "Opportunity Name: " + result.Name +" ,\r\n  Account : " + result.TargetPartyName + ". The customer " + result.TargetPartyName + " is at high risk.\r\n Would you like to know more details like revenue, churn index or what is in the news about the account?";
+                    var suggests = [{ "title" : "What is the status"},{ "title" : "What is the churn index"},{ "title" : "What is in the news"}];
                     
                     if (req.body.originalRequest.source == "google") {
                         res.json({
