716a53a1f3d87a474f0865fb98771a7952058d6d
Gokul Nair <gokulgnair94@gmail.com>
2017-10-02 20:46:10 +0530

    Top Opty

--- server.js
+++ server.js
@@ -818,7 +818,7 @@ restService.post('/opptytop', function(req, res) {
                     
                     //console.log( "result : " + JSON.stringify(result));
                     speech = "Opportunity Name: " + result.Name +" ,  Account : " + result.TargetPartyName + ". The customer " + result.TargetPartyName + " is at high risk. Would you like to know more details like revenue, churn index or what is in the news about the account. ";
-                    var suggests = [{ "title" : ""},{ "title" : "What is the revevnue"},{ "title" : "What is the churn index"},{ "title" : "News about " + result.TargetPartyName }];
+                    var suggests = [{ "title" : ""},{ "title" : "What is the revevnue"},{ "title" : "What is the churn index"},{ "title" : "What is in the news"}];
                     
                     if (req.body.originalRequest.source == "google") {
                         res.json({
