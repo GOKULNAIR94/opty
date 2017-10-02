ccfe5e70ac0ec1c47f9f3cba9544539ff17a0c26
Gokul Nair <gokulgnair94@gmail.com>
2017-10-03 00:50:37 +0530

    Top Opty

--- server.js
+++ server.js
@@ -527,7 +527,7 @@ restService.post('/oppty', function(req, res) {
                                     var contactPhone = resObj.PrimaryFormattedPhoneNumber;
 
                                     speech = 'Here are the details for Activity: ' + activityNumber + ',\n\r Subject: ' + subject + ',\n\r Status: ' + status + ',\n\r Start Date: ' + mydate(startDate, "yyyy-mm-dd") + ',\n\r End Date: ' + mydate(endDate, "yyyy-mm-dd") + ',\n\r Opportunity Associated: ' + optyName + ',\n\r Customer Name: ' + contactName + ',\n\r Phone: ' + contactPhone + ',\n\r Email: ' + contactEmail + ',\n\r Account: ' + AccountName + ".\n Would you like to know the churn index or what is in the news about " + AccountName + ", or would you like to close this activity?";
-                                    var suggests = [{ "title" : "Close this activity"},{ "title" : "What is the churn index"},{ "title" : "What is in the news"}];
+                                    var suggests = [{ "title" : "Get me news"},{ "title" : "What is the churn index"},{ "title" : "Close this activity"}];
                                     
                                     if (req.body.originalRequest.source == "google") {
                                         res.json({
