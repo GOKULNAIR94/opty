18bf3a4ac085c81d6c14e6583f636d6ffec22fb2
Gokul Nair <gokulgnair94@gmail.com>
2018-04-19 00:29:29 +0530

    rel 13

--- activity.js
+++ activity.js
@@ -1,49 +1,52 @@
-module.exports = function ( qString, loginEncoded, req, resp, callback){ 
-    var http = require("https");
-    console.log( "qString : " + qString);
-    var options = {
-      "method": "GET",
-      "hostname": "acs.fa.ap2.oraclecloud.com",
-      "port": null,
-      "path": qString, //customFields.CO.Serial_Number%3D'C355'%20AND%20statusWithType.status.lookupName%3D'Solved'
-      "headers": {
-        "authorization": "Basic TE5UMDAxOmxudExOVDJLMTZfMQ==",//loginEncoded,
-        "cache-control": "no-cache"
-      }
-    };
+module.exports = function(req, res, callback) {
+    var Query = require("./query");
 
-    var req = http.request(options, function (res) {
-      var responseString = '',
-            resObj;
+    var qString = "";
+    var rowCount = 0;
 
-      res.on("data", function ( data ) {
-        responseString += data;
-      });
+    qString = "/crmRestApi/resources/latest/activities?q=OwnerName=Akashdeep%20Makkar&onlyData=true";
+    
+    Query( qString, req.body.headers.authorization, req, res, function( result ){
+        rowCount = result.count;
 
-      res.on("end", function () {
-          try{
-              //console.log("Body : " + responseString);
-          resObj = JSON.parse(responseString);
-          if( resObj.items != null ){
-              var rowCount = resObj.items.length;
-              console.log( "rowCount : " + rowCount);
-              //console.log( "resObj : " + JSON.stringify(resObj));
-          }
-          //console.log( "resObj : " + JSON.stringify(resObj));
-          callback( resObj );
-          }
-          catch(e){
-              resp.json({
-                message : "Error: " + e 
+        for (var i = 0; i <= rowCount - 1; i++) {
+                
+                if (result.items[i].ActivityNumber != null && result.items[i].ActivityNumber != "") {
+                    speech = speech + 'Activity Number: ' + result.items[i].ActivityNumber + ', Subject: ' + result.items[i].Subject + ';\r\n';
+                    suggests.push({
+                        "title": result.items[i].ActivityNumber
+                    })
+                }
+                console.log(speech);
+        }
+        if (req.body.originalRequest.source == "google") {
+            res.json({
+                speech: speech,
+                displayText: speech,
+                //contextOut : [{"name":"oppty-followup","lifespan":5,"parameters":{"objType":"activities"}}],
+                data: {
+                    google: {
+                        'expectUserResponse': true,
+                        'isSsml': false,
+                        'noInputPrompts': [],
+                        'richResponse': {
+                            'items': [{
+                                'simpleResponse': {
+                                    'textToSpeech': speech,
+                                    'displayText': speech
+                                }
+                            }],
+                            "suggestions": suggests
+                        }
+                    }
+                }
             });
-          }
-      });
-    res.on("error", function ( e ) {
-        resp.json({
-                message : "Error: " + e 
+        }else{
+            res.json({
+                speech: speech,
+                displayText: speech
             });
-      });
+        }
     });
 
-    req.end();
 }
